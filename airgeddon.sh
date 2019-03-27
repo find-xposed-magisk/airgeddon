50addc77278b772113c6f494c21702dc8ee6e0ef
xpz3 <tcaller111@gmail.com>
2019-03-28 01:29:39 +0530

    Evil Twin AP Attack With Sniffing And SSLStrip - Added function call to kill required tmux windows after successful completion of the attack.

--- airgeddon.sh
+++ airgeddon.sh
@@ -7506,6 +7506,7 @@ function exec_et_sniffing_sslstrip_attack() {
 	language_strings "${language}" 115 "read"
 
 	kill_et_windows
+	kill_tmux_windows
 	if [ "${dos_pursuit_mode}" -eq 1 ]; then
 		recover_current_channel
 	fi
