39a9b8f601cc0c8b9f850a3964f7b1ecd8c2c5fb
Magix <3617098+magixgf@users.noreply.github.com>
2019-10-02 20:06:07 +0200

    Plugin template updated with override and prehook stuff

--- plugins/plugin_template.sh
+++ plugins/plugin_template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Global shellcheck disabled warnings
-#shellcheck disable=SC2034
+#shellcheck disable=SC2034,SC2154
 
 #Generic plugin vars
 plugin_name="Set your plugin name here"
@@ -17,3 +17,27 @@ plugin_minimum_ag_affected_version="10.0"
 plugin_maximum_ag_affected_version=""
 #Set only one element in the array "*" to affect all distros, otherwise add them one by one with the name which airgeddon uses for that distro (examples "BlackArch", "Parrot")
 plugin_distros_affected=("*")
+
+#To override airgeddon functions, just define them following this nomenclature name:
+#<plugin_name>_override_<function_name>
+#plugin_name: This is the name of the plugin filename without extension (.sh)
+#function_name: This is the name of the airgeddon function you want to rewrite with new content
+
+#Overridden function example
+#This will change echo_blue function to make it print in red color
+function plugin_template_override_echo_blue() {
+
+	#If you want in addition to add a pre-hook to the same function you are overriding, you need to set a call to hook_and_debug
+	#Not needed if you just want to override
+	hook_and_debug
+
+	last_echo "${1}" "${red_color}"
+}
+
+#Prehook function example
+#This will execute this content before the echo_blue function
+#If you want to prehook a function which at the same time is going to be overridden, you need to add the hook_and_debug call into the overridden function
+function plugin_template_prehook_echo_blue() {
+
+	echo "************** Prehooked function - We are going to print in red!! *************"
+}
