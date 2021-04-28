9fc60484abc71b93dd0cf01cfa9802c3a4a24388
v1s1t0r1sh3r3 <v1s1t0r.1s.h3r3@gmail.com>
2021-04-29 01:11:00 +0200

    Prepare version numbers for v10.42

--- airgeddon.sh
+++ airgeddon.sh
@@ -2,7 +2,7 @@
 #Title........: airgeddon.sh
 #Description..: This is a multi-use bash script for Linux systems to audit wireless networks.
 #Author.......: v1s1t0r
-#Version......: 10.41
+#Version......: 10.42
 #Usage........: bash airgeddon.sh
 #Bash Version.: 4.2 or later
 
@@ -131,8 +131,8 @@ declare -A possible_alias_names=(
 								)
 
 #General vars
-airgeddon_version="10.41"
-language_strings_expected_version="10.41-1"
+airgeddon_version="10.42"
+language_strings_expected_version="10.42-1"
 standardhandshake_filename="handshake-01.cap"
 standardpmkid_filename="pmkid_hash.txt"
 standardpmkidcap_filename="pmkid.cap"
