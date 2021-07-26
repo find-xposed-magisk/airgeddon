1a17fce89124f9e51619189a7f53ae70dac014f2
v1s1t0r1sh3r3 <v1s1t0r.1s.h3r3@gmail.com>
2021-07-27 01:21:26 +0200

    Implement busy ports check for Evil Twin with sniffing and bettercap-sslstrip2 attack

--- airgeddon.sh
+++ airgeddon.sh
@@ -6133,8 +6133,15 @@ function evil_twin_attacks_menu() {
 						fi
 
 						if [ "${et_attack_adapter_prerequisites_ok}" -eq 1 ]; then
-							et_mode="et_sniffing_sslstrip2"
-							et_dos_menu
+
+							declare -gA ports_needed
+							ports_needed["tcp"]="8080"
+							ports_needed["udp"]="67"
+							if check_busy_ports; then
+
+								et_mode="et_sniffing_sslstrip2"
+								et_dos_menu
+							fi
 						fi
 					fi
 				else
